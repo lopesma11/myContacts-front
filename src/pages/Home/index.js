ce18de5d5e6cf02f44472f847797486bd317cecc
lopesma11 <ml722090@gmail.com>
2025-02-26 09:46:22 -0300

    task: implementing ContactService

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -11,7 +11,7 @@ import trash from "../../assets/images/icons/trash-red.svg";
 import edit from "../../assets/images/icons/note-pencil-blue.svg";
 import { Link } from "react-router-dom";
 import Loader from "../../components/Loader";
-import delay from "../../utils/delay.js";
+import ContactsService from "../../services/ContactsService.js";
 
 export default function Home() {
     const [contacts, setContacts] = useState([]);
@@ -32,14 +32,10 @@ export default function Home() {
             try {
                 setIsLoading(true);
 
-                const response = await fetch(
-                    `http://localhost:3001/contacts?orderBy=${orderBy}`
+                const contactsList = await ContactsService.listContacts(
+                    orderBy
                 );
-
-                await delay(500);
-
-                const json = await response.json();
-                setContacts(json);
+                setContacts(contactsList);
             } catch (error) {
                 console.log("error", error);
             } finally {
