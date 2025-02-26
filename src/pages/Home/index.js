e9c272ae3311e47c1ae033ed1ae7fac3f5099d0f
lopesma11 <ml722090@gmail.com>
2025-02-25 22:06:36 -0300

    task: working with async:await inside of useEffect

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -28,20 +28,26 @@ export default function Home() {
     );
 
     useEffect(() => {
-        setIsLoading(true);
-        fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`)
-            .then(async (response) => {
+        async function loadContacts() {
+            try {
+                setIsLoading(true);
+
+                const response = await fetch(
+                    `http://localhost:3001/contacts?orderBy=${orderBy}`
+                );
+
                 await delay(500);
 
                 const json = await response.json();
                 setContacts(json);
-            })
-            .catch((error) => {
-                console.log("erro", error);
-            })
-            .finally(() => {
+            } catch (error) {
+                console.log("error", error);
+            } finally {
                 setIsLoading(false);
-            });
+            }
+        }
+
+        loadContacts();
     }, [orderBy]);
 
     const handleToogleOrderBy = () => {
