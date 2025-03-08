ea54931349c0c84330f6444cc1d46463bacb3c9a
lopesma11 <ml722090@gmail.com>
2025-03-08 12:33:41 -0300

    task: implementing contact creation

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types";
 import useErrors from "../../hooks/useErrors";
 import CategoriesService from "../../services/CategoriesService";
 
-export default function ContactForm({ buttonLabel }) {
+export default function ContactForm({ buttonLabel, onSubmit }) {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [phone, setPhone] = useState("");
@@ -65,7 +65,7 @@ export default function ContactForm({ buttonLabel }) {
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        console.log({
+        onSubmit({
             name,
             email,
             phone,
@@ -130,4 +130,5 @@ export default function ContactForm({ buttonLabel }) {
 
 ContactForm.propTypes = {
     buttonLabel: PropTypes.string.isRequired,
+    onSubmit: PropTypes.func.isRequired,
 };
