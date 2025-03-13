92a5ca5408eb240036e7a667826fbddae7c060fe
lopesma11 <ml722090@gmail.com>
2025-03-13 11:29:17 -0300

    task: implementing the event listener removal method

---
+++ src/lib/EventManager.js
@@ -0,0 +1,33 @@
+export default class EventManager {
+    constructor() {
+        this.listeners = {};
+    }
+
+    on(event, listener) {
+        if (!this.listeners[event]) {
+            this.listeners[event] = [];
+        }
+
+        this.listeners[event].push(listener);
+    }
+
+    emit(event, payload) {
+        if (!this.listeners[event]) return;
+
+        this.listeners[event].array.forEach((listener) => {
+            listener(payload);
+        });
+    }
+}
+
+const toastEventManager = new EventManager();
+
+toastEventManager.on("addtoast", (payload) => {
+    console.log("addtoast listener1", payload);
+});
+
+toastEventManager.on("addtoast", (payload) => {
+    console.log("addtoast listener2", payload);
+});
+
+toastEventManager.emit("addtoast", () => {});
