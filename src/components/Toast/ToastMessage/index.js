7dec85d1643ef41321fc642868935df44da4e5bb
lopesma11 <ml722090@gmail.com>
2025-03-12 21:10:36 -0300

    task: working with variants at Styled Components

--- src/components/Toast/ToastMessage/index.js
+++ src/components/Toast/ToastMessage/index.js
@@ -6,7 +6,7 @@ import xCircleIcon from "../../../assets/images/icons/x-circle.svg";
 
 export default function ToastMessage({ text, type }) {
     return (
-        <Container>
+        <Container type={type}>
             {type === "danger" && <img src={xCircleIcon} alt="xCircleIcon" />}
             {type === "success" && (
                 <img src={checkCircleIcon} alt="checkCircleIcon" />
