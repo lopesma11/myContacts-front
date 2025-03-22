55353a030809029ca33d8fc68739636cac425a90
lopesma11 <ml722090@gmail.com>
2025-03-22 14:17:04 -0300

    task: loading contact data on the edit page

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PageHeader from "../../components/PageHeader";
 import ContactForm from "../../components/ContactForm";
+import { useParams, useHistory } from "react-router-dom";
+import ContactsService from "../../services/ContactsService";
+import Loader from "../../components/Loader";
+import toast from "../../utils/toast";
 
 export default function EditContact() {
+    const [isLoading, setIsLoading] = useState(true);
+    const { id } = useParams();
+    const history = useHistory();
+
+    useEffect(() => {
+        async function loadContact() {
+            try {
+                const contactData = await ContactsService.getContactById(id);
+
+                console.log(contactData);
+                setIsLoading(false);
+            } catch {
+                history.push("/");
+                toast({
+                    type: "danger",
+                    text: "Contato não encontrado!",
+                });
+            }
+        }
+
+        loadContact();
+    }, [id, history]);
+
+    function handleSubmit() {}
+
     return (
         <>
+            <Loader isLoading={isLoading} />
             <PageHeader title="Editar Matheus Lopes" />
-            <ContactForm buttonLabel="Salvar Alterações"></ContactForm>
+            <ContactForm
+                buttonLabel="Salvar Alterações"
+                onSubmit={handleSubmit}
+            ></ContactForm>
         </>
     );
 }
