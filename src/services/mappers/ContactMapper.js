79d424f0bed7e31f1e0057cf1cd42af3f7fe17af
lopesma11 <ml722090@gmail.com>
2025-04-16 11:59:32 -0300

    feat: implementing the toDomain from ContactMapper

--- src/services/mappers/ContactMapper.js
+++ src/services/mappers/ContactMapper.js
@@ -9,7 +9,16 @@ class ContactMapper {
     }
 
     toDomain(persistenceContact) {
-        return {};
+        return {
+            id: persistenceContact.id,
+            name: persistenceContact.name,
+            email: persistenceContact.email,
+            phone: persistenceContact.phone,
+            category: {
+                id: persistenceContact.category_id,
+                name: persistenceContact.category_name,
+            },
+        };
     }
 }
 
