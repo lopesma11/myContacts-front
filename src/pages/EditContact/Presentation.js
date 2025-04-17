e098a31e89e06f1f44b9d297d4d6b3d8becf7603
lopesma11 <ml722090@gmail.com>
2025-04-16 21:46:42 -0300

    feat: implementing ContainerPresentational

---
+++ src/pages/EditContact/Presentation.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+import PageHeader from "../../components/PageHeader";
+import ContactForm from "../../components/ContactForm";
+import Loader from "../../components/Loader";
+
+export default function Presentation({
+    isLoading,
+    contactName,
+    contactFormRef,
+    onSubmit,
+}) {
+    return (
+        <>
+            <Loader isLoading={isLoading} />
+            <PageHeader
+                title={
+                    isLoading
+                        ? "Carregando contato..."
+                        : `Editar ${contactName}`
+                }
+            />
+            <ContactForm
+                ref={contactFormRef}
+                buttonLabel="Salvar Alterações"
+                onSubmit={onSubmit}
+            ></ContactForm>
+        </>
+    );
+}
+
+Presentation.propTypes = {
+    isLoading: PropTypes.bool.isRequired,
+    contactName: PropTypes.string.isRequired,
+    contactFormRef: PropTypes.shape().isRequired,
+    onSubmit: PropTypes.func.isRequired,
+};
