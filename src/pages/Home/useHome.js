7dc7437a092f2dfc2af5c3829984bfabc8906678
lopesma11 <ml722090@gmail.com>
2025-05-16 11:39:18 -0300

    feat: improvement legibility at Home Page

--- src/pages/Home/useHome.js
+++ src/pages/Home/useHome.js
@@ -31,6 +31,7 @@ export default function useHome() {
         } catch (error) {
             console.log("Caiu no catch", error);
             setHasError(true);
+            setContacts([]);
         } finally {
             setIsLoading(false);
         }
