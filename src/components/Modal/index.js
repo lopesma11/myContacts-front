2a5fb8dd57e70946fee16088e052b5c21681a6a2
lopesma11 <ml722090@gmail.com>
2025-06-20 11:39:28 -0300

    feat: implementing modal exit animation with setimeout

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Overlay, Container, Footer } from "./styles";
 import Button from "../Button";
 import PropTypes from "prop-types";
@@ -15,7 +15,27 @@ export default function Modal({
     onCancel,
     onConfirm,
 }) {
-    if (!visible) {
+    const [shouldRender, setShouldRender] = useState(visible);
+
+    useEffect(() => {
+        if (visible) {
+            setShouldRender(true);
+        }
+
+        let timeoutId;
+
+        if (!visible) {
+            timeoutId = setTimeout(() => {
+                setShouldRender(false);
+            }, 300);
+        }
+
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, [visible]);
+
+    if (!shouldRender) {
         return null;
     }
 
@@ -29,8 +49,8 @@ export default function Modal({
 
     return (
         <ReactPortal containerId="modal-root">
-            <Overlay>
-                <Container danger={danger}>
+            <Overlay isLeaving={!visible}>
+                <Container danger={danger} isLeaving={!visible}>
                     <h1>{title}</h1>
                     <div className="modal-body">{children}</div>
                     <Footer>
--- src/pages/Home/useHome.js
+++ src/pages/Home/useHome.js
@@ -60,7 +60,6 @@ export default function useHome() {
 
     function handleCloseDeleteModal() {
         setIsDeleteModalVisible(false);
-        setContacBeingDeleted(null);
     }
 
     async function handleConfirmDeleteContact() {
