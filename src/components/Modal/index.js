7d7b367c9ebb68917ce39cc7d056b19274c09141
lopesma11 <ml722090@gmail.com>
2025-06-26 20:07:02 -0300

    feat: creating useAnimatedUnmount Hook

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import { Overlay, Container, Footer } from "./styles";
 import Button from "../Button";
 import PropTypes from "prop-types";
 import ReactPortal from "../ReactPortal";
+import useAnimatedUnmounted from "../../hooks/useAnimatedUnmount";
 
 export default function Modal({
     danger,
@@ -15,37 +16,7 @@ export default function Modal({
     onCancel,
     onConfirm,
 }) {
-    const [shouldRender, setShouldRender] = useState(visible);
-
-    const overlayRef = useRef(null);
-
-    useEffect(() => {
-        if (visible) {
-            setShouldRender(true);
-        }
-
-        function handleAnimationEnd() {
-            setShouldRender(false);
-        }
-
-        const overlayRefElement = overlayRef.current;
-
-        if (!visible && overlayRefElement) {
-            overlayRefElement.addEventListener(
-                "animationend",
-                handleAnimationEnd
-            );
-        }
-
-        return () => {
-            if (overlayRefElement) {
-                overlayRefElement.removeEventListener(
-                    "animationend",
-                    handleAnimationEnd
-                );
-            }
-        };
-    }, [visible]);
+    const { shouldRender, animatedElementRef } = useAnimatedUnmounted(visible);
 
     if (!shouldRender) {
         return null;
@@ -61,7 +32,7 @@ export default function Modal({
 
     return (
         <ReactPortal containerId="modal-root">
-            <Overlay isLeaving={!visible} ref={overlayRef}>
+            <Overlay isLeaving={!visible} ref={animatedElementRef}>
                 <Container danger={danger} isLeaving={!visible}>
                     <h1>{title}</h1>
                     <div className="modal-body">{children}</div>
---
+++ src/hooks/useAnimatedUnmount.js
@@ -0,0 +1,37 @@
+import { useState, useRef, useEffect } from "react";
+
+export default function useAnimatedUnmounted({ visible }) {
+    const [shouldRender, setShouldRender] = useState(visible);
+
+    const animatedElementRef = useRef(null);
+
+    useEffect(() => {
+        if (visible) {
+            setShouldRender(true);
+        }
+
+        function handleAnimationEnd() {
+            setShouldRender(false);
+        }
+
+        const elementRef = animatedElementRef.current;
+
+        if (!visible && elementRef) {
+            elementRef.addEventListener("animationend", handleAnimationEnd);
+        }
+
+        return () => {
+            if (elementRef) {
+                elementRef.removeEventListener(
+                    "animationend",
+                    handleAnimationEnd
+                );
+            }
+        };
+    }, [visible]);
+
+    return {
+        shouldRender,
+        animatedElementRef,
+    };
+}
