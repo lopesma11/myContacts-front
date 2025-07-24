f37584e3abd762a3e8731680d50715433ce79337
lopesma11 <ml722090@gmail.com>
2025-07-24 11:01:45 -0300

    feat: animating the entrance of components on list

--- src/components/Toast/ToastMessage/styles.js
+++ src/components/Toast/ToastMessage/styles.js
@@ -1,4 +1,15 @@
-import styled, { css } from "styled-components";
+import styled, { css, keyframes } from "styled-components";
+
+const messageIn = keyframes`
+from {
+    opacity: 0;
+    transform: translateY(100px);
+}
+
+to {
+    opacity: 1;
+    transform: translateY(0px);
+}`;
 
 const containerVariants = {
     default: css`
@@ -21,10 +32,9 @@ export const Container = styled.div`
     align-items: center;
     justify-content: center;
     cursor: pointer;
-
-    ${({ type }) => containerVariants[type] || containerVariants.default}
-
-    & + & {
+    animation: ${messageIn} 0.3s
+        ${({ type }) => containerVariants[type] || containerVariants.default} & +
+        & {
         margin-top: 12px;
     }
 
